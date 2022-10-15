3659ebce6d027596a8ab6860861acd5634a66a6a
r-puccinelli <78498975+r-puccinelli@users.noreply.github.com>
2022-10-14 23:47:32 -0300

    Add slash

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, {configType}) => {
     if(configType == "PRODUCTION") {
-      config.base = '/ignite-ds'
+      config.base = '/ignite-ds/'
     }
 
     return config
